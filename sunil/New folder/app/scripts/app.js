380f12e3e3966b31cf41914d9e6637c33cbfab80
vinodh18 <vinodhkumar.bandaru123@gmail.com>
2017-11-18 11:11:58 -0800

    atminton

---
+++ sunil/New folder/app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('batmintonApp', [
+  'ngCookies',
+  'ngRoute'
+])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/main',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+      
+    $locationProvider.html5Mode(true);
+  });
